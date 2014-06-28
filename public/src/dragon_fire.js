04fcab9cf5e209c8ec5d7cafe11bb4ada4ae8426
Ashima Athri <ashimaathri@gmail.com>
2014-06-27 23:09:52 -0400

    remove the require

--- public/src/dragon_fire.js
+++ public/src/dragon_fire.js
@@ -1,51 +1,49 @@
-require([], function () {
-  Q.Sprite.extend("DragonFire",{
-    init: function(p) {
-      this._super(p, {
-        sheet: "bossfire",
-        distance: 0,
-        scale: 0.5,
-        originalY: p.y,
-        vy: 0,
-        gravity: 0,
-        sensor: true
-      });
+Q.Sprite.extend("DragonFire",{
+  init: function(p) {
+    this._super(p, {
+      sheet: "bossfire",
+      distance: 0,
+      scale: 0.5,
+      originalY: p.y,
+      vy: 0,
+      gravity: 0,
+      sensor: true
+    });
 
-      if (this.p.vx < 0){
-        this.p.flip = "x";
-      }
-      this.add("2d, animation");
-      this.on("sensor");
+    if (this.p.vx < 0){
+      this.p.flip = "x";
+    }
+    this.add("2d, animation");
+    this.on("sensor");
 
-      this.on('hit',function(collision) {
-        if (!collision.obj.isA('Dragon')){
-          this.destroy();
-        }
-      });
-    },
-    sensor: function (obj) {
-        if (obj.isA('Boss')) {
-          return;
-        }
-
-        if(obj.isA("Player")) {
-          Q.audio.play('/sounds/mario_die.wav');
-          Q.stageScene("endGame",1, { label: "You Died" });
-          obj.destroy();
-          this.destroy();
-        }
-
-        if (obj.isA("Enemy")) {
-          this.destroy();
-        }
-    },
-    step: function (dt) {
-      // this.p.y = this.p.originalY - 5 ;
-      this.p.distance += 1;
-
-      if (this.p.distance === 100) {
+    this.on('hit',function(collision) {
+      if (!collision.obj.isA('Dragon')){
         this.destroy();
       }
+    });
+  },
+  sensor: function (obj) {
+      if (obj.isA('Boss')) {
+        return;
+      }
+
+      if(obj.isA("Player")) {
+        Q.audio.play('/sounds/mario_die.wav');
+        Q.stageScene("endGame",1, { label: "You Died" });
+        obj.destroy();
+        this.destroy();
+      }
+
+      if (obj.isA("Enemy")) {
+        this.destroy();
+      }
+  },
+  step: function (dt) {
+    // this.p.y = this.p.originalY - 5 ;
+    this.p.distance += 1;
+
+    if (this.p.distance === 100) {
+      this.destroy();
     }
-  });
+  }
 });
