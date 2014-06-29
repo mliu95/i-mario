92c98a2d127bcb837e875b902e93d99faef65aed
Michael Liu <michael.liu95@gmail.com>
2014-06-29 11:11:34 -0400

    working on multiplayer

--- public/src/players.js
+++ public/src/players.js
@@ -3,11 +3,7 @@ require(['./src/fireball'], function () {
     init: function(p, hash) {
       this._super(p, hash);
 
-      this.add('2d, platformerControls, animation');
-
-      Q.input.on('fire', this, 'fireWeapon');
-
-      this.on('hit.sprite',function (collision) {
+      this.on('hit',function (collision) {
         if (collision.obj.isA('Mashroom')){
           this.p.health += 200;
           if (this.p.health > 500){
@@ -23,8 +19,6 @@ require(['./src/fireball'], function () {
           Q.audio.play('/sounds/gulp.wav');
         }
       });
-
-      this.on('damage', 'onDamage');
     },
     insertHealthDisplay: function () {
       var hd = new Q.HealthDisplay();
@@ -48,7 +42,7 @@ require(['./src/fireball'], function () {
         return;
       }
 
-      if(this.p.bullets) {
+      if(this.p.bullets && this.p.fireWait === 0) {
         if (this.p.direction === 'left') {
           this.stage.insert(new Q.Fireball({ x: this.p.x - 15, y: this.p.y, vx: -250 , power: this.p.power}));
           // this.stage.insert(new Q.Fireball({ x: this.p.x - 15, y: this.p.y + 15, vx: -250, power: this.p.power }));
@@ -58,11 +52,17 @@ require(['./src/fireball'], function () {
           // this.stage.insert(new Q.Fireball({ x: this.p.x + 15, y: this.p.y + 15, vx: 250, power: this.p.power }));
           this.p.bullets--;
         }
+        this.p.fireWait++;
+        Q.state.set("bullets", this.p.bullets);
+        Q.audio.play('/sounds/fireball.wav');
      }
-      Q.state.set("bullets", this.p.bullets);
-      Q.audio.play('/sounds/fireball.wav');
     },
     step: function (dt) {
+      if(this.p.fireWait >= 5){
+        this.p.fireWait = 0;
+      } else if (this.p.fireWait >= 1){
+        this.p.fireWait ++;
+      }
       if(!this.p.healthDisplay){
         this.insertHealthDisplay();
       }
@@ -98,11 +98,59 @@ require(['./src/fireball'], function () {
         jumpInput: false,
         originalHealth: 500,
         scale: 0.07,
+        fireWait: 0,
         bullets: 0,
         power: 10
       });
       this.className = 'Player';
       Q.state.set("health", this.p.health);
+      this.add('2d, platformerControls, animation');
+
+      Q.input.on('fire', this, 'fireWeapon');
+
+      this.on('damage', 'onDamage');
+    }
+  });
+  Q.Player.extend('Ghost',{
+    init: function(p) {
+      this._super(p, {
+        sheet: 'player',
+        sprite: 'player',
+        flip: 'x',
+        health: 100,
+        jumpInput: false,
+        originalHealth: 500,
+        scale: 0.07,
+        fireWait: 0,
+        bullets: 0,
+        power: 10
+      });
+      this.className = 'Ghost';
+      Q.state.set("health", this.p.health);
+      this.add('2d, animation');
+    },
+    step: function (dt) {
+      if(!this.p.healthDisplay){
+        this.insertHealthDisplay();
+      }
+      if(this.p.jumping && !this.p.jumpInput){
+        this.p.jumpInput = true;
+        Q.audio.play('/sounds/jump.wav', {"debounce": 500});
+      } else if(!this.p.jumping && this.p.vy === 0){
+        this.p.jumpInput = false;
+      }
+      if(this.p.y > 1000) {
+        this.destroy();
+        this.p.healthDisplay.destroy();
+      } else if(this.p.vx > 0) {
+        this.p.flip='x';
+        this.play('run_right');
+      } else if(this.p.vx < 0) {
+        this.p.flip='';
+        this.play('run_left');
+      } else {
+        this.play('stand_' + this.p.direction);
+      }
     }
   });
 });
